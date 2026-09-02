589269fc2becf9f9bc725662b6c8d94c9b754ae8
Радмир <radmirmixa@gmail.com>
2026-09-02 07:36:11 +0300

    Complete homework 0

---
+++ homeworks/hw00/hello.py
@@ -0,0 +1,9 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+
+print("Версия Python:", sys.version)
+print("Версия NumPy:", np.__version__)
+print("Версия Pandas:", pd.__version__)
+print("Версия Matplotlib:", matplotlib.__version__)
